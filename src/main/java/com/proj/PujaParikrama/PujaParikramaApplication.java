104a450e2b27cb432af9e73b11714ad4aadfb52e
Arnab Saha <arnabsaha2506@gmail.com>
2026-09-04 13:43:00 +0530

    Initial Commit

---
+++ src/main/java/com/proj/PujaParikrama/PujaParikramaApplication.java
@@ -0,0 +1,13 @@
+package com.proj.PujaParikrama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PujaParikramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PujaParikramaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proj/PujaParikrama/PujaParikramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.PujaParikrama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PujaParikramaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
